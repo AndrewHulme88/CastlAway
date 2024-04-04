eb8189e9220aa4ab842b92f957b88fb486d8fdfd
William Pettersen <williamkpettersen@outlook.com>
2024-04-04 20:17:26 +1100

    broom cursor

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -5,3 +5,12 @@ export default class extends Controller {
     this.element.textContent = "Hello World!";
   }
 }
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
