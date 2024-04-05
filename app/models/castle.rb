319e308c3fa2360e6cb03e98244ce9c180078a15
andrew hulme <andrew_hulme04@hotmail.com>
2024-04-05 23:06:14 +1100

    Implemented back end for geocoding

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -4,4 +4,6 @@ class Castle < ApplicationRecord
   has_many :favourites
   has_many :favourited_by, through: :favourites, source: :user
   validates :name, :location, :price, :description, :rating, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
