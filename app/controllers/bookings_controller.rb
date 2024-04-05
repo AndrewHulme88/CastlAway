a07ef4432dbfac7b89668a56ab45c918fbf0896f
ginpan <zgpan7@gmail.com>
2024-04-06 02:17:38 +1100

    Added new and create actions in controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @castle = Castle.find(params[:castle_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @castle = Castle.find(params[:castle_id])
+    @booking = current_user.bookings(booking_params)
+    @booking.castle = @castle
+
+    if @booking.save
+      redirect_to @castle, notice: 'Booking was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:to, :from, :total_price, :castle_id, :user_id)
+  end
 end
